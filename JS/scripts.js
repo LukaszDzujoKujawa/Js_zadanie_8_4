6ec2a6ca46e92fd888900c079fac75b2ddcc75fd
Lukasz Kujawa <lukasz.kujawa@interia.pl>
2018-04-16 22:36:53 +0200

    add comments JS

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,5 +1,20 @@
 // scripts.js
 
 var name = prompt('Enter your name');
+
+/*
+	comment alert
+*/
+
+
 alert('Hello, ' + name);
+
+/*
+	second comment 
+*/
+
 console.log('Hello, ' + name);
+
+/*
+	third comment 
+*/
