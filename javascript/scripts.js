ddcaa81c4dbf420153e1bc59f5afcd0393f6c98e
Lukasz Kujawa <lukasz.kujawa@interia.pl>
2018-05-04 23:19:34 +0200

    edit javascript

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -2,10 +2,8 @@ var name = prompt('Enter your name');
 
 //alert('Hello, ' + name);
 
-alert('Hello, ' + name);
-
 //	second comment 
 
-console.log('Hello, ' + name);
-
 //	third comment 
+
+console.log('Hello, ' + name);
